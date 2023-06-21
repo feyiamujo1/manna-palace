b5df0f902887aa340fa80f9901d68c7eb52a0c86
feyiamujo1 <feyisayoamujoyegbe@gmail.com>
2023-06-21 18:09:39 +0100

    Started user section

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,49 @@
 import Image from 'next/image'
+import { GiShoppingBag } from "react-icons/gi"
+import { IoPersonSharp } from "react-icons/io5"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className='w-full relative'>
+      <nav className='absolute top-0 left-0 right-0 w-full bg-transparent-black py-2 shadow-lg'>
+        <div className='w-11/12 mx-auto flex justify-between items-center'>
+          <Image src="/logo.png" alt="logo" height={40} width={90} />
+          <div className='flex gap-3 items-center'>
+            {/* <IoPersonSharp className=' cursor-pointer fill-white text-2xl hover:fill-custom-yellow' /> */}
+            <GiShoppingBag className=' cursor-pointer fill-white text-2xl hover:fill-custom-yellow' />
+          </div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      </nav>
+      <section className=' h-screen w-full bg-hero-image bg-top md:bg-center bg-cover bg-no-repeat flex justify-center items-center'>
+        <div className='text-center space-y-3 mt-20 md:mt-32'>
+          <Image src="/title-shape.png" width={150} height={30} alt='' className='mx-auto object-cover' />
+          <div>
+            <h1 className='font-black text-4xl sm:text-5xl md:text-7xl text-custom-yellow'>Manna Palace</h1>
+            <p className='font-semibold text-custom-yellow -mb-1'>No more long queues or waiting times!</p>
+          </div>
+          <p className='text-white font-semibold'>Redeemer's University's premier food ordering website</p>
+          <p className='border-custom-yellow border py-2 px-4 w-fit font-semibold text-white mx-auto cursor-pointer hover:bg-custom-yellow transition-all'>View Menu</p>
+        </div>
+      </section>
+      <section className=' min-h-screen py-10 bg-slate-100'>
+        <div className='w-11/12 mx-auto space-y-6'>
+          <div className=' space-y-2'>
+            <Image src="/title-shape.png" width={150} height={30} alt='' className='mx-auto object-cover' />
+            <h2 className=' font-extrabold text-2xl md:text-3xl text-custom-yellow text-center'>Our Menu</h2>
+            <p className='text-center'>
+              Browse through the menu, select your desired items, customize your order, and securely place it with just a few clicks.
+            </p>
+          </div>
+          <div>
+            <div className='flex gap-2 mx-auto w-fit py-2 px-3 text-sm bg-[#000] text-slate-300 rounded-xl border border-custom-yellow'>
+              <p className='bg-[#1A2F33] py-2 px-3 rounded-lg cursor-pointer'>Meals</p>
+              <p className='bg-[#000] py-2 px-3 rounded-lg cursor-pointer'>Swallow</p>
+              <p className='bg-[#000] py-2 px-3 rounded-lg cursor-pointer'>Snacks</p>
+              <p className='bg-[#000] py-2 px-3 rounded-lg cursor-pointer'>Drinks</p>
+            </div>
+          </div>
+        </div>
+      </section>
     </main>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,13 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        "custom-yellow": "#E47513",
+        "light-custom-yellow": "#FFA02A",
+        "transparent-black": "rgb(0, 0, 0, 0.7)"
+      },
       backgroundImage: {
+        'hero-image': "linear-gradient(rgba(0, 0, 41, 0.5), rgba(2, 19, 10, 0.9)) , url('/gourmet-chicken-biryani-with-steamed-basmati-rice-generated-by-ai.jpg')",
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
